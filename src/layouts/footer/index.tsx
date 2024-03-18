aa5b91d201373e5b7e52101aa9499a4489a8ae50
mattfranc <matt.franc25@gmail.com>
2024-03-19 04:42:12 +0800

    updating footer

--- src/layouts/footer/index.tsx
+++ src/layouts/footer/index.tsx
@@ -1,48 +1,94 @@
 'use client'
-import dynamic from 'next/dynamic'
-import { Box, Grid, Stack, Typography } from '@mui/material'
-import { CONSTANT } from '@/utils/constants'
-
-const StudentIcon = dynamic(() => import('@/assets/icons/student.svg'))
-const BookIcon = dynamic(() => import('@/assets/icons/book.svg'))
-const GrowIcon = dynamic(() => import('@/assets/icons/grow.svg'))
-const ServiceCard = dynamic(() => import('@/components/service-card'))
-
-export const serviceData = [
-  {
-    title: "Easy Wins",
-    subtitle: "Get your best looking smile now!",
-    icon: <StudentIcon />,
-  },
-  {
-    title: "Concrete",
-    subtitle: "Defalcate is most focused in helping you discover your most beautiful smile",
-    icon: <BookIcon />,
-  },
-  {
-    title: "Hack Growth",
-    subtitle: "Overcame any hurdle or any other problem.",
-    icon: <GrowIcon />,
-  },
-];
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Footer = () => {
   return (
-    <>
-      <Box component={'section'} maxWidth={{ sm: "720px", md: "1148px" }} marginX={"auto"} px={{xs: '2rem', md: '1rem'}} py={'80px'}>
-        <Stack gap={1.25} textAlign={'center'} mb={3}>
-          <Typography variant='h5' color={'secGray.light'} sx={{ fontWeight: 400 }}>{CONSTANT.TEXT.FEATURED_SERVICE}</Typography>
-          <Typography variant='h4' color={'secGray.main'}>{CONSTANT.TEXT.BESTSELLER_SERVICE}</Typography>
-          <Typography variant='h6' color={'secGray.light'} sx={{ fontWeight: 400 }}>{CONSTANT.TEXT.BESTSELLER_SERVICE_SUBTITLE}</Typography>
-        </Stack>
-        <Grid container columns={{ xs: 1, md: 5 }} gap={3.75} justifyContent={'center'} alignItems={'center'} p={{ xs: 3, md: 4 }}>
-          {serviceData.map((item: any, i: number) => (
-            <ServiceCard key={i} title={item.title} subtitle={item.subtitle} icon={item.icon} />
-          ))}
+    <Box sx={{ bgcolor: 'background.paper', color: 'text.primary', py: 6 }}>
+      <Container maxWidth="lg">
+        <Grid container xs={12} justifyContent="space-between">
+          <Typography variant="h4" gutterBottom>
+            Bandage
+          </Typography>
+          <Box sx={{ display: { md: 'block' } }}>
+            {/* Social icons for desktop */}
+            <IconButton aria-label="Facebook" color="primary">
+              <FacebookIcon />
+            </IconButton>
+            <IconButton aria-label="Instagram" color="primary">
+              <InstagramIcon />
+            </IconButton>
+            <IconButton aria-label="Twitter" color="primary">
+              <TwitterIcon />
+            </IconButton>
+          </Box>
         </Grid>
-      </Box>
-    </>
-  )
-}
+        <Grid container spacing={5} justifyContent="space-between">
+          <Grid item xs={12} sm={6} md={3}>
+          <Typography variant="h6" gutterBottom>
+            Company Info
+          </Typography>
+          <Link href="#" color="inherit">About Us</Link><br />
+          <Link href="#" color="inherit">Carrier</Link><br />
+          <Link href="#" color="inherit">We are hiring</Link><br />
+          <Link href="#" color="inherit">Blog</Link>
+          </Grid>
+          <Grid item xs={10} sm={6} md={2}>
+            <Typography variant="h6" gutterBottom>
+              Legal
+            </Typography>
+            <Link href="#" color="inherit">About Us</Link><br />
+            <Link href="#" color="inherit">Carrier</Link><br />
+            <Link href="#" color="inherit">We are hiring</Link><br />
+            <Link href="#" color="inherit">Blog</Link>
+          </Grid>
+          <Grid item xs={10} sm={6} md={2}>
+            <Typography variant="h6" gutterBottom>
+              Features
+            </Typography>
+            <Link href="#" color="inherit">Business Marketing</Link><br />
+            <Link href="#" color="inherit">User Analytic</Link><br />
+            <Link href="#" color="inherit">Live Chat</Link><br />
+            <Link href="#" color="inherit">Unlimited Support  </Link>
+          </Grid>
+          <Grid item xs={10} sm={6} md={2}>
+            <Typography variant="h6" gutterBottom>
+              Resources
+            </Typography>
+            <Link href="#" color="inherit">IOS & Android</Link><br />
+            <Link href="#" color="inherit">Watch a Demo</Link><br />
+            <Link href="#" color="inherit">Customers</Link><br />
+            <Link href="#" color="inherit">API</Link>
+          </Grid>
 
-export default Footer
+          <Grid item xs={10} sm={6} md={2} justifyContent="center">
+            <Typography variant="h6" gutterBottom>
+              Get In Touch
+            </Typography>
+            <TextField fullWidth placeholder="Your Email" />
+            <Button variant="contained" sx={{ mt: 1, width: '100%' }}>
+              Subscribe
+            </Button>
+          </Grid>
+        </Grid>
+
+
+        <Typography variant="subtitle1" align="left" sx={{ mt: 3 }}>
+          Made With Love By Finland All Rights Reserved
+        </Typography>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
+
